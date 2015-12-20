bf183ceb2acaa83a07cafd254c12ac02cb5eba7f
Carlos Salas <devel@reserland.com>
2015-12-20 13:15:45 +0100

    Renombrado paquete de actions

--- src/java/controller/actions/TestActions.java
+++ src/java/controller/actions/TestActions.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.actions;
+package controller.actions;
 
 import com.itextpdf.text.DocumentException;
 import controller.DoTestServlet;
